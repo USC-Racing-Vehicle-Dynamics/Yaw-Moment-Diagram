cf9c30d3c3f5eea40e31880369f2e090dde2a4bc
lewistple <tianpail@usc.edu>
2025-05-17 10:16:50 -0700

    Update export file

--- YMD (v6.2)/YMD_export.m
+++ YMD (v6.2)/YMD_export.m
@@ -1,6 +1,6 @@
 %% Function: Export Data
 % Export data to a .mat file
-function YMD_exportData(prompt)
+function YMD_export(prompt)
 
 % Obtain vehicle parameters & YMD inputs
 param = evalin('base', 'param');
@@ -45,9 +45,9 @@ switch prompt
     
     case 3 % Export data (sweep plot)
 
-        Ax = evalin('base', 'AxSweepData');
-        Ay = evalin('base', 'AySweepData');
-        M = evalin('base', 'MSweepData');
+        Ax = evalin('base', 'AxData_sweep');
+        Ay = evalin('base', 'AyData_sweep');
+        M = evalin('base', 'MData_sweep');
         sweptParameter = evalin('base', 'sweptParamInfo');
 
         cd ..
@@ -55,12 +55,59 @@ switch prompt
         status = mkdir(folderName);
         
         % Save as .mat File
-        exportField = evalin('base', 'editFields.exportSweep');
+        exportField = evalin('base', 'editFields.exportSweepDataName');
         fileName = get(exportField, 'value');
         save(folderName + "/" + fileName + ".mat", 'param', 'sweptParameter', 'SA', 'Delta', 'SX', 'Ax', 'Ay', 'M');
 
         cd 'YMD (v6.2)'
+    
+    case 4 % Export figure (sweep plot)
 
+        YMD = evalin('base', 'panels.YMDSweepArea');
+
+        cd ..
+        folderName = 'YMD Sweep Results';
+        status = mkdir(folderName);
+        
+        % Save as .fig
+        exportField = evalin('base', 'editFields.exportSweepFigName');
+        figName = get(exportField, 'value');
+        exportgraphics(YMD, folderName + "/" + figName + ".jpg");
+
+        cd 'YMD (v6.2)'
+
+    case 5 % Export data (GGV plot)
+
+        Ax = evalin('base', 'AxData_GGV');
+        Ay = evalin('base', 'AyData_GGV');
+        V = evalin('base', 'V.range');
+
+        cd ..
+        folderName = 'GGV Results';
+        status = mkdir(folderName);
+
+        % Save as .mat File
+        exportField = evalin('base', 'editFields.exportGGVDataName');
+        fileName = get(exportField, 'value');
+        save(folderName + "/" + fileName + ".mat", 'param', 'SA', 'Delta', 'SX', 'Ax', 'Ay', 'V');
+
+        cd 'YMD (v6.2)'
+
+    case 6 % Export figure (GGV plot)
+
+        YMD = evalin('base', 'panels.GGVArea');
+
+        cd ..
+        folderName = 'GGV Results';
+        status = mkdir(folderName);
+        
+        % Save as .fig
+        exportField = evalin('base', 'editFields.exportGGVFigName');
+        figName = get(exportField, 'value');
+        exportgraphics(YMD, folderName + "/" + figName + ".jpg");
+
+        cd 'YMD (v6.2)'
+        
 end
 
 end
